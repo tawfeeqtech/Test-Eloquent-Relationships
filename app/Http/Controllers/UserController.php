59e7e8d50b479d49a8775f5d25fb8fbae3777d22
tawfeeqtech <86443941+tawfeeqtech@users.noreply.github.com>
2024-07-30 12:35:34 +0300

    end

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $users = User::all();
-        $users = User::with('projects')->has('projects')->get();
+        $users = User::has('projects')->get();
 
         return view('users.index', compact('users'));
     }
